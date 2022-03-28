05c19e7713dbc02bafb39e6b417d62e35f9bf156
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-03-28 08:58:23 -0400

    render courses in the template

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+  // 1. Render courses in a list
+  // 2. Select a course
+  // 3. Render selected course
   courses = [
     {
       id: 1,
@@ -13,12 +16,29 @@ export class CoursesComponent implements OnInit {
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
       favorite: true
+    },
+    {
+      id: 2,
+      title: 'Javascript The HARDEST PARTS EVER',
+      description: 'Learn the Javascript like a pro! with Will',
+      percentComplete: 26,
+      favorite: true
     }
   ];
+
+  selectedCourse = null;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectCourse(course) {
+    this.selectedCourse = course;
+  }
+
+  deleteCourse(courseId) {
+    console.log('DELETE COURSE', courseId);
   }
 
 }
