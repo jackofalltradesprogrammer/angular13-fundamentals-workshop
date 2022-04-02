f1feea143eab5f5ec70db95edb27bad519028be2
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2022-04-02 13:13:11 -0400

    course container + presentation containers

---
+++ src/app/courses/courses-details/courses-details.component.ts
@@ -0,0 +1,13 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Course } from 'src/app/common/models/course';
+
+@Component({
+  selector: 'app-courses-details',
+  templateUrl: './courses-details.component.html',
+  styleUrls: ['./courses-details.component.scss']
+})
+export class CoursesDetailsComponent {
+  @Input() currentCourse: Course;
+  @Output() saved = new EventEmitter();
+  @Output() cancelled = new EventEmitter();
+}
